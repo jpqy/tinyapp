7ca1c55d52d34af14735b138bfccb2dc1ec4cf0a
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-16 17:16:15 -0400

    routing for /urls/<shortURL>

--- express_server.js
+++ express_server.js
@@ -21,11 +21,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get('/urls', (req, res)=>{
+app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
-
+});
+app.get('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  let templateVars = { shortURL, longURL };
+  res.render('urls_show', templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
