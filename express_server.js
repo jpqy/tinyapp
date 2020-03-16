9764037a17bee286d0a83caec29c11b9f654be69
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-16 18:15:50 -0400

    added url/new

--- express_server.js
+++ express_server.js
@@ -25,6 +25,10 @@ app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
