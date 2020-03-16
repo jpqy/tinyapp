283987f62a42747a40bed62dda5a8499eac85428
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-16 19:04:20 -0400

    redirect after submitting new url

--- express_server.js
+++ express_server.js
@@ -39,8 +39,9 @@ app.get('/urls/:shortURL', (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-    urlDatabase[generateRandomString()] = req.body.longURL;
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
