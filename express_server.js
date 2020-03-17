9392281662279da56245f105df556788037f7e0e
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-03-17 16:52:54 -0400

    handle POST to /login

--- express_server.js
+++ express_server.js
@@ -62,6 +62,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect("/urls");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
