f6bab6c0e8316f68edeb419d019fc43dda7674b6
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-03-17 17:19:12 -0400

    added login logic and logout

--- express_server.js
+++ express_server.js
@@ -70,6 +70,12 @@ app.post("/login", (req, res) => {
   res.cookie('username', username);
   res.redirect("/urls");
 });
+
+app.post('/logout', (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
