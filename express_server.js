1a3925ef6218ac4028502e4e10c488d8b3ae026d
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-03-18 15:10:02 -0400

    handles logins

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -38,20 +38,20 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get('/urls', (req, res) => {
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase, user: users[(req.cookies["user_id"])] };
   res.render("urls_index", templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new', { user: users[(req.cookies["user_id"])] });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new", { user: users[(req.cookies["user_id"])] });
 });
 
-app.get('/urls/:shortURL', (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   let templateVars = { shortURL, longURL, user: users[(req.cookies["user_id"])] };
-  res.render('urls_show', templateVars);
+  res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -78,43 +78,52 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
-app.get('/register', (req, res) => {
-  res.render('register', { user: users[req.cookies["user_id"]] });
+app.get("/register", (req, res) => {
+  res.render("register", { user: users[req.cookies["user_id"]] });
 });
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    res.status(400).send('You did not fill out the form correctly!');
+    res.status(400).send("You did not fill out the form correctly!");
   } else if (getIdFromEmail(email)) {
-    res.status(400).send('Email already in use!');
+    res.status(400).send("Email already in use!");
   }
   const id = generateRandomString();
   const newUser = { id, email, password };
   users[id] = newUser;
-  res.cookie('user_id', id);
-  res.redirect('/urls');
+  res.cookie("user_id", id);
+  res.redirect("/urls");
 });
 
-app.get('/login', (req, res) => {
-  res.render('login', { user: users[req.cookies["user_id"]] });
+app.get("/login", (req, res) => {
+  res.render("login", { user: users[req.cookies["user_id"]] });
 });
 
-app.post('/login', (req, res) => {
-  res.send('TODO');
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const id = getIdFromEmail(email);
+  if (!id) {
+    res.status(403).send("Login failed");
+  } else if (users[id].password !== password) {
+    res.status(403).send("Login failed");
+  } else {
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+  }
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 function generateRandomString() {
-  let string = '';
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let string = "";
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charLength = chars.length;
 
   for (let i = 0; i < 6; i++) {
